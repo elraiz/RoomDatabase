d6a1727b2e388d23a36dd29acb649a1241b89a38
elraiz <raizrock161@gmail.com>
2020-05-01 14:23:35 +0700

    Initial commit

---
+++ app/src/main/java/com/elraiz/roomdatabase/AppDatabase.java
@@ -0,0 +1,11 @@
+package com.elraiz.roomdatabase;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AktivisDao aktivisDao();
+}
